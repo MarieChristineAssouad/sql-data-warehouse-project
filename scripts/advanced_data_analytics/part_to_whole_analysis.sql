305953240551ee6c306b26be2a2ff7ec86d7c56e
MarieChristineAssouad <mariechristineassouad@gmail.com>
2025-11-19 19:16:05 -0500

    Update part_to_whole_analysis.sql

--- scripts/advanced_data_analytics/part_to_whole_analysis.sql
+++ scripts/advanced_data_analytics/part_to_whole_analysis.sql
@@ -31,12 +31,6 @@ SELECT
       category,
       total_sales,
       SUM(total_sales) OVER () AS overall_sales,
-      CONCAT(
-          ROUND(
-              (CAST(total_sales AS FLOAT) 
-                 / SUM(total_sales) OVER()) * 100,
-              2
-          ), '%'
-      ) AS percentage_of_total
+      CONCAT( ROUND( (CAST(total_sales AS FLOAT) / SUM(total_sales) OVER()) * 100, 2 ), '%') AS percentage_of_total
 FROM category_sales
 ORDER BY total_sales DESC;

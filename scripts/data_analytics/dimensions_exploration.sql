f9576cd5c2964d00670185b32f0f61a7545efd14
MarieChristineAssouad <mariechristineassouad@gmail.com>
2025-11-19 22:57:11 -0500

    Update dimensions_exploration.sql

--- scripts/data_analytics/dimensions_exploration.sql
+++ scripts/data_analytics/dimensions_exploration.sql
@@ -1,45 +1,18 @@
 /* 
-===============================================================
+_______________________________________________________________
                DIMENSIONS EXPLORATION SCRIPT
-===============================================================
+_______________________________________________________________
 
 This script provides a quick overview of key dimension tables 
 to understand customer distribution and the structure of the 
-product catalog. I typically run these checks early in a project 
-to confirm available attributes and validate the quality of 
-dimension data before building analytics or modeling pipelines.
+product catalog. */
 
----------------------------------------------------------------
-          1. CUSTOMER GEOGRAPHY EXPLORATION
----------------------------------------------------------------
-
-Returns all distinct customer countries.  
-This helps reveal geographic reach, regional patterns, and 
-potential segmentation opportunities.
-*/
-
-/* 
-============================================================
-               DISTINCT CUSTOMER COUNTRIES
-============================================================ */
+--DISTINCT CUSTOMER COUNTRIES 
 SELECT DISTINCT 
     country
 FROM gold.dim_customers;
 
-/* 
-============================================================
-         PRODUCT CATEGORY & SUBCATEGORY STRUCTURE
-============================================================ */
-/*
-This section extracts the unique combinations of:
-  - Category
-  - Subcategory
-  - Product name
-
-It gives a clear view of the major groupings in the product 
-catalog and is useful for verifying hierarchy consistency.
-*/
-
+-- PRODUCT CATEGORY & SUBCATEGORY STRUCTURE
 SELECT DISTINCT 
       category,
       subcategory,
